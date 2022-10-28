da47a06362d32bdf631b76d872066c378c4ce474
Alexander <114307680+Alexander37ss@users.noreply.github.com>
2022-10-27 19:20:19 -0600

    Add files via upload

---
+++ parcial2/PracticaWebDatos/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica de web con base de datos</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/jquery-3.6.1.js"></script>
+</head>
+<body>
+    <?php include 'menu.php';
+    <?php include 'conexion.php';
+    
+    ?>
+    
+    <br>
+    <div></div>
+</body>
+</html>
